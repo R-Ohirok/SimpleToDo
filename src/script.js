b0d7061cdaeb23d7e73fb89b54d3306ed953aefb
Roman Ohirok <matsuhita.2307@gmail.com>
2025-06-16 16:17:07 +0300

    delete code that is not relevant to the task

--- src/script.js
+++ src/script.js
@@ -47,10 +47,6 @@
 
       const deleteBtn = document.createElement('button');
       deleteBtn.textContent = 'D';
-      deleteBtn.addEventListener('click', () => {
-        todoList.removeChild(li);
-        todoList.removeChild(hr);
-      });
 
       right.appendChild(edit);
       right.appendChild(deleteBtn);
