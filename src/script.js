0371869d4db8289967a765447b76a819f54491de
Roman Ohirok <matsuhita.2307@gmail.com>
2025-06-17 11:20:27 +0300

    Add status filtering functionality

--- src/script.js
+++ src/script.js
@@ -1,9 +1,83 @@
-  const modal = document.getElementById('modal');
   const addBtn = document.querySelector('.add-btn');
-  const cancelBtn = document.getElementById('cancelBtn');
+  const modal = document.getElementById('modal');
   const noteInput = document.getElementById('noteInput');
   const applyBtn = document.getElementById('applyBtn');
+  const cancelBtn = document.getElementById('cancelBtn');
   const todoList = document.querySelector('.todo-list');
+  const filterStatus = document.getElementById('filter');
+
+  let todos = [];
+  let activeFilterStatus = filterStatus.value;
+
+  function generateId() {
+    return Math.random().toString(36).slice(0, 8);
+  }
+
+  function showTodo(activeTodo) {
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = activeTodo.isTaskComleted;
+    checkbox.name = activeTodo.taskId;
+    checkbox.className = 'todo-item__status';
+    checkbox.addEventListener('change', () => {
+      li.classList.toggle('todo-item__completed', checkbox.checked);
+      activeTodo.isTaskComleted = !activeTodo.isTaskComleted;
+    });
+
+    const label = document.createElement('span');
+    label.className = 'todo-item__title';
+
+    label.textContent = activeTodo.taskLabel;
+    
+    li.appendChild(checkbox);
+    li.appendChild(label);
+
+    const control = document.createElement('div');
+    control.className = 'todo-item__control';
+
+    const edit = document.createElement('button');
+    edit.textContent = 'E';
+    edit.className = 'todo-item__control-btn';
+
+    const deleteBtn = document.createElement('button');
+    
+    deleteBtn.textContent = 'D';
+    deleteBtn.addEventListener('click', () => {
+      todoList.removeChild(li);
+      todos = todos.filter(todo => todo.taskId !== activeTodo.taskId);
+      renderTodos();
+    });
+    deleteBtn.className = 'todo-item__control-btn';
+
+    control.appendChild(edit);
+    control.appendChild(deleteBtn);
+
+    li.appendChild(control);
+
+    todoList.appendChild(li);
+  }
+
+  function renderTodos() {
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+      if (
+        (activeFilterStatus === 'All') || 
+        (activeFilterStatus === 'Active' && !todo.isTaskComleted) || 
+        (activeFilterStatus === 'Completed' && todo.isTaskComleted)
+      ) {
+        showTodo(todo);
+      }
+    });
+  }
+
+  filterStatus.addEventListener('change', () => {
+    activeFilterStatus = filterStatus.value;
+    renderTodos();
+  });
 
   addBtn.addEventListener('click', () => {
     modal.classList.remove('hidden');
@@ -16,52 +90,16 @@
   });
 
   applyBtn.addEventListener('click', () => {
-    const task = noteInput.value.trim();
-    if (task !== '') {
+    const taskLabel = noteInput.value.trim();
+
+    if (taskLabel !== '') {
+      const isTaskComleted = false;
+      const taskId = generateId();
+
+      todos.push({taskId, taskLabel, isTaskComleted});
       
-      const li = document.createElement('li');
-      li.className = 'todo-item';
-
-      const left = document.createElement('div');
-      left.className = 'todo-item__left';
-
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.addEventListener('change', () => {
-        li.classList.toggle('todo-item__completed', checkbox.checked);
-      });
-
-      const label = document.createElement('span');
-      label.textContent = task;
-      
-      left.appendChild(checkbox);
-      left.appendChild(label);
-      
-      li.appendChild(left);
-
-      const right = document.createElement('div');
-      right.className = 'todo-item__right';
-
-      const edit = document.createElement('button');
-      edit.textContent = 'E';
-
-      const deleteBtn = document.createElement('button');
-      
-      deleteBtn.textContent = 'D';
-      deleteBtn.addEventListener('click', () => {
-        todoList.removeChild(li);
-        todoList.removeChild(hr);
-      });
-
-
-
-      right.appendChild(edit);
-      right.appendChild(deleteBtn);
-
-      li.appendChild(right);
-
-      todoList.appendChild(li);
-
       modal.classList.add('hidden');
+      
+      renderTodos();
     }
   });
